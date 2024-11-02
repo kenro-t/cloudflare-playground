40e9c95f504a9d5d6ac9222f99cb10ca8123c567
kenro-t <ngkn1425@gmail.com>
2024-11-02 06:06:59 +0000

    test

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-export const runtime = 'edge';
+// export const runtime = 'edge';
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
